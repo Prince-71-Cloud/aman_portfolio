3442556aca0746c54c441a90097a017d54ed3e41
Prince-71-Cloud <bhuiyanaman71@gmail.com>
2025-12-09 09:56:02 +0600

    Fix: Update base path for GitHub Pages project site

--- _next/static/gB_QaNncNR_pi1kPZHB77/_buildManifest.js
+++ _next/static/gB_QaNncNR_pi1kPZHB77/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/aman_portfolio//_next/:path+",
+        "destination": "/aman_portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
